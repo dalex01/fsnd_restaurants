fe483c4abe62a083c3900b2f47cc863afeccc997
Alexey Drozdov <alexey.drozdov@gmail.com>
2015-11-29 23:53:23 +0300

    Add templates for restaurants

--- restaurants/views_restaurants.py
+++ restaurants/views_restaurants.py
@@ -9,3 +9,20 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+@app.route('/restaurants/')
+def restaurants():
+    restaurants = session.query(Restaurant).all()
+    return render_template('restaurants.html', restaurants = restaurants)
+
+@app.route('/restaurants/new/')
+def newRestaurantItem():
+    return render_template('newrestaurantitem.html')
+
+@app.route('/restaurants/<int:restaurant_id>/edit/')
+def editRestaurantItem(restaurant_id):
+    return render_template('editrestaurantitem.html', restaurant_id=restaurant_id)
+
+@app.route('/restaurants/<int:restaurant_id>/delete/')
+def deleteRestaurantItem(restaurant_id):
+    return render_template('deleterestaurantitem.html', restaurant_id=restaurant_id)
