d2efcc5cc127527132f67265d87a04e62287e89c
Alexey Drozdov <alexey.drozdov@gmail.com>
2015-12-07 22:47:53 +0300

    Add SeaSurf module logic to prevent cross-site request forgery attacks

--- restaurants/__init__.py
+++ restaurants/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask.ext.seasurf import SeaSurf
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024
+csrf = SeaSurf(app)
 
 import restaurants.views
